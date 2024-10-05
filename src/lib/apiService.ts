af20d3eb38687c4922a0966892ff7644de842d66
Gstx <geekhous3@outlook.com>
2024-10-05 19:02:14 -0300

    removed

--- src/lib/apiService.ts
+++ src/lib/apiService.ts
@@ -11,7 +11,6 @@ export function getToken() {
 
 export function getApiClient() {
     const token = getToken();
-    // return hc<AppType>("https://uck0sss8ok0ggcc4scok4k4w.revenantcheats.com", {
     return hc<AppType>("http://localhost:3000/", {
         headers: token && typeof token === 'string' ? {
             Authorization: `Bearer ${token}`,
