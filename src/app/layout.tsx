d4b9a0519c4f3261152416d019ae14f33ecb53e9
Gstx <geekhous3@outlook.com>
2024-10-05 12:00:57 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import "./globals.css";
-import localFont from "next/font/local";
 import { Toaster } from "@/components/ui/toaster"
 export const metadata: Metadata = {
   title: 'Parada dos sabores',
