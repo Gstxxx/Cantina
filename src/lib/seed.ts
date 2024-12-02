86ba102cff3ac84ae5ccc2bc02b3b82143f8d1e8
Gstxxx <geekhous3@outlook.com>
2024-12-02 20:21:58 -0300

    feat(fix seed)

--- src/lib/seed.ts
+++ src/lib/seed.ts
@@ -12,44 +12,44 @@ export async function seed() {
             type: 'Admin'
         }
     });
-    for (let i = 1; i <= 100; i++) {
-        await prisma.client.create({
-            data: {
-                name: faker.person.fullName(),
-                phone: faker.phone.number({ style: 'national' })
-            }
-        });
-    }
+    // for (let i = 1; i <= 100; i++) {
+    //     await prisma.client.create({
+    //         data: {
+    //             name: faker.person.fullName(),
+    //             phone: faker.phone.number({ style: 'national' })
+    //         }
+    //     });
+    // }
 
-    const foods = ['Arroz', 'Feijão', 'Macarrão', 'Carne', 'Frango', 'Peixe', 'Salada', 'Sopa', 'Pizza', 'Hambúrguer', 'Cachorro-quente', 'Taco', 'Burrito', 'Sushi', 'Sashimi', 'Tempurá', 'Yakissoba', 'Guioza', 'Harumaki', 'Coxinha', 'Pastel', 'Esfiha', 'Kibe', 'Tabule', 'Quibebe', 'Baião de dois', 'Vatapá', 'Moqueca', 'Bobó de camarão', 'Caruru'];
-    for (let i = 0; i < foods.length; i++) {
-        await prisma.product.create({
-            data: {
-                name: foods[i],
-                price: Math.floor(Math.random() * 1000) + 100
-            }
-        });
-    }
+    // const foods = ['Arroz', 'Feijão', 'Macarrão', 'Carne', 'Frango', 'Peixe', 'Salada', 'Sopa', 'Pizza', 'Hambúrguer', 'Cachorro-quente', 'Taco', 'Burrito', 'Sushi', 'Sashimi', 'Tempurá', 'Yakissoba', 'Guioza', 'Harumaki', 'Coxinha', 'Pastel', 'Esfiha', 'Kibe', 'Tabule', 'Quibebe', 'Baião de dois', 'Vatapá', 'Moqueca', 'Bobó de camarão', 'Caruru'];
+    // for (let i = 0; i < foods.length; i++) {
+    //     await prisma.product.create({
+    //         data: {
+    //             name: foods[i],
+    //             price: Math.floor(Math.random() * 1000) + 100
+    //         }
+    //     });
+    // }
 
-    for (let i = 1; i <= 250; i++) {
-        const randomClientId = Math.floor(Math.random() * 100) + 1;
-        const randomProductId = Math.floor(Math.random() * foods.length) + 1; 
-        const randomQuantity = Math.floor(Math.random() * 10) + 1;
-        const randomDay = Math.floor(Math.random() * 31) + 1;
+    // for (let i = 1; i <= 250; i++) {
+    //     const randomClientId = Math.floor(Math.random() * 100) + 1;
+        // const randomProductId = Math.floor(Math.random() * foods.length) + 1; 
+        // const randomQuantity = Math.floor(Math.random() * 10) + 1;
+        // const randomDay = Math.floor(Math.random() * 31) + 1;
 
-        await prisma.purchaseRecord.create({
-            data: {
-                clientId: randomClientId,
-                purchaseDate: new Date(`2024-10-${randomDay.toString().padStart(2, '0')}`),
-                products: {
-                    create: {
-                        productId: randomProductId,
-                        quantity: randomQuantity
-                    }
-                }
-            }
-        });
-    }
+        // await prisma.purchaseRecord.create({
+        //     data: {
+        //         clientId: randomClientId,
+        //         purchaseDate: new Date(`2024-10-${randomDay.toString().padStart(2, '0')}`),
+    //             products: {
+    //                 create: {
+    //                     productId: randomProductId,
+    //                     quantity: randomQuantity
+    //                 }
+    //             }
+    //         }
+    //     });
+    // }
 }
 
 seed();
